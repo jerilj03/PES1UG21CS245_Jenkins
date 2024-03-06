16286334ba90bc9818d006bb26097ea25ed2d6fc
jerilj03 <jeriljoshy03@gmail.com>
2024-03-06 21:27:22 +0530

    cpp file changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am PES1UG21CS245\n";
+  cout << "am PES1UG21CS245\n";
   return 0;
 }
